385c8cac4290d7f3e9ddbeb3690dbfd5d201b1d1
Derya Tanriverdi <deryatanriverdi_@hotmail.com>
2019-07-30 06:12:49 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -1,63 +1,6 @@
 def first_challenge
-<<<<<<< HEAD
 epic_tragedy = {	
    :montague => {},	  
    :capulet => {}	
-=======
-epic_tragedy = {
-  :montague => {
-    :patriarch => {
-      :name => "",
-      :age => "53"
-  },
-    :matriarch => {
-      :name => "Lady Montague",
-      :age => "54"
-  },
-    :hero => {
-      :name => "Romeo",
-      :age => "15"
-  },
-    :hero_friends => [
-      {
-         :name => "Benvolio",
-         :age => "17",
-         :attitude => "worried"
-      },
-      {
-        :name => "Mercutio",
-        :age => "18",
-        :attitude => "hot-headed"
-      }
-    ]
-  },
-  :capulet => {
-    :patriarch => {
-      :name => "Lord Capulet",
-      :age => "50"
-    },
-    :matriarch => {
-      :name => "Lady Capulet",
-      :age => "51"
-    },
-    :heroine => {
-      :name => "Juliet",
-      :age => "15",
-      :status => "alive"
-    },
-    :heroine_friends=> [
-      {
-        :name => "Steven",
-        :age => "30",
-        :attitude => "confused"
-      },
-      {
-        :name => "Nurse",
-        :age => "44",
-        :attitude => "worried"
-      }
-    ]
-  }
->>>>>>> ddcb5b761f7383d63dea86365230eb173bff3235
 }
 end
